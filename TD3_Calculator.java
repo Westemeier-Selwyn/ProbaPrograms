b02aa35aac369f611430328906dd9a2eeb935d12
Selwyn Westemeier <selwyn.westemeier.001@student.uni.lu>
2022-04-21 18:44:36 +0200

    Done

--- TD3_Calculator.java
+++ TD3_Calculator.java
@@ -1,6 +1,9 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TD3_Calculator {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
 
     public static void main(String[] args) {
         
@@ -10,12 +13,7 @@ public class TD3_Calculator {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter Page of exercise: (1,2,3,4,5) / 0 = exit");
             int page = sc.nextInt();
-            while(page > 5 || page < 0)
-            {
-                System.out.println("TG decken");
-                page = 0;
-                page = sc.nextInt();
-            }
+            
             switch (page)
             {
                 case 0:
@@ -36,8 +34,8 @@ public class TD3_Calculator {
                     double p2 = sc.nextDouble();
 
                     double ii = n0*p0 + n1*p1 + n2*p2;
-                    double iii = (n0*n0*p0) + (n1*n1*p1) * (n2*n2*p2);
-                    double iv = (ii*ii) - iii;
+                    double iii = (n0*n0*p0) + (n1*n1*p1) + (n2*n2*p2);
+                    double iv = iii - (ii*ii);
 
                     System.out.println("(ii) = " + ii);
                     System.out.println("(iii) = " + iii);
@@ -59,8 +57,8 @@ public class TD3_Calculator {
                     System.out.println("If failed:");
                     int perte = sc.nextInt()*mise;
                     
-                    double gain = m1*(1/6) + m2*(5/36) + m3*(25/216) + perte*(125/216) - mise;
-                    System.out.println("Your gain is " + gain + " €.");
+                    double gain = (m1*36 + m2*5*6 + m3*25 + perte*125)/216;
+                    System.out.println("Your gain is " + df.format(gain) + " Euro.");
                 }
                 break;
 
@@ -74,16 +72,57 @@ public class TD3_Calculator {
                     double chances = 0;
                     for(int i = 0; i < revolver; i++)
                     {
-                        chances += success*Math.pow(echec, i);
+                        chances += (success*Math.pow(echec, i))*(i+1);
                         if(i == (revolver-1))
                         {
-                            chances = Math.pow(echec, i+1);
+                            chances += (Math.pow(echec, i+1))*(i+1);
                         }
                     }
+                    System.out.println("Average shots = " + df.format(chances));
                 }
                 break;
-                
+
+                case 4:
+                {
+                    System.out.println("(i) Dice Number: ");
+                    int diceNr = sc.nextInt();
+                    double i =  diceNr*0.16666666666;
+                    System.out.println("Average = " + df.format(i));
+                    System.out.println("(ii) Probability: ");
+                    double probability = sc.nextDouble();
+                    double ii = 1/(1-probability);
+                    System.out.println("Probability = " + df.format(ii));
+                }
+                break;
+
+                case 5:
+                {
+                    System.out.println("Total of children: ");
+                    int children = sc.nextInt();
+                    System.out.println("Total of boys: ");
+                    int boysNr = sc.nextInt();
+                    System.out.println("Chances of having boys: ");
+                    double probability = sc.nextDouble();
+                    double i = Math.pow(probability, boysNr)*Math.pow(1-probability, children-boysNr);
+                    System.out.println("Probability of exactly " + boysNr + " boys = " + df.format(i));
+                    double ii = 0;
+                    int o = 0;
+                    for(int j = children; j > (children-boysNr); j--)
+                    {
+                        ii += Math.pow(1-probability, j)*Math.pow(probability, o);
+                        o++;
+                    }
+                    System.out.println("(ii) Probability of less boys than girls = " + df.format(ii));
+                }
+                break;
+
+                default:
+                {
+                    System.out.println("TG decken");
+                }
+                break;
             }
+
         }
 
     }
